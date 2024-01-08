02755e2341547d97bf5299e57197858a4c48f30f
Lee Sol <leeso4567@naver.com>
2024-01-08 21:19:53 +0900

    2024-01-08 프로그래머스 Lv.0 단계 솔브

--- Programmers_Lv_0/SumBinaryCode.js
+++ Programmers_Lv_0/SumBinaryCode.js
@@ -4,7 +4,7 @@ function solution(bin1, bin2) {
   let sum = parseInt(bin1, 2) + parseInt(bin2, 2);
   let remainder = 0;
 
-  if (sum === 0) return 0;
+  if (sum === 0) return "0";
   else {
     while (sum > 0) {
       remainder = sum % 2;
