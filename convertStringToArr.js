cedd6093f760ec84d3fdde032802067e2cb18802
Lee Sol <leeso4567@naver.com>
2024-12-04 00:31:26 +0900

    숫자 문자열 num 수 만큼 쪼갠 배열로 만드는 컨버터 함수 구현

--- convertStringToArr.js
+++ convertStringToArr.js
@@ -1,16 +1,19 @@
-function convertStringToArr(num, string) {
+function convertStringToNumArr(num, string) {
   let tmpArr = string.split(" ");
 
+  let tmpArrToNumArr = tmpArr.map((item) => parseInt(item));
+
   let array = new Array(num);
+
   for (let i = 0; i < num; i++) {
     array[i] = new Array(num);
   }
 
-  for (let i = 0; i < num; i++) {
-    array[i].push(tmpArr.slice(i, num * (i + 1)));
+  for (let i = 0; i < tmpArrToNumArr.length; i = i + num) {
+    array[i / num] = tmpArrToNumArr.slice(i, i + num);
   }
 
   console.log(array);
 }
 
-convertStringToArr(5, "2 3 1 7 3 4 1 9 6 8 5 5 2 4 4 6 5 2 6 7 8 4 2 2 2");
+convertStringToNumArr(5, "2 3 1 7 3 4 1 9 6 8 5 5 2 4 4 6 5 2 6 7 8 4 2 2 2");
